225896718ba780ad5f5f3c5817fd61305bb04da1
E. Kolpakov <eugeny@opencraft.com>
2017-04-23 13:04:53 +0800

    Added (dummy) tests and test infrastructure

---
+++ tests/unit/test_dummy.py
@@ -0,0 +1,23 @@
+import unittest
+
+import pytest
+
+
+def test_addition():
+    assert 1+1 == 2
+
+
+def test_substraction():
+    assert 1-1 == 0
+
+
+@pytest.mark.parametrize(
+    "operand1, operand2, result",
+    [
+        (1, 1, 2),
+        (42, 18, 60),
+        (7, 1, 8)
+    ]
+)
+def test_parameterized_addition(operand1, operand2, result):
+    assert operand1 + operand2 == result
