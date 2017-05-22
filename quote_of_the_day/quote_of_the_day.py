5b008c84775852b9649296ec64b2f713a24cb123
E. Kolpakov <eugeny@opencraft.com>
2017-05-22 17:06:48 +0200

    Adding to favorites: XBlock field and handler

--- quote_of_the_day/quote_of_the_day.py
+++ quote_of_the_day/quote_of_the_day.py
@@ -1,8 +1,9 @@
 """TO-DO: Write a description of what this XBlock is."""
+from __future__ import unicode_literals
 
 import pkg_resources
 from xblock.core import XBlock
-from xblock.fields import Integer, Scope
+from xblock.fields import List, Scope
 from xblock.fragment import Fragment
 
 from read_quote import read_quote
@@ -11,20 +12,25 @@ class QuoteOfTheDayXBlock(XBlock):
     """
     TO-DO: document what your XBlock does.
     """
-
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
+    favorites = List(
+        name = "Favorite quotes",
+        scope = Scope.user_state,
     )
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
+
+    def _render_favorites(self):
+        favorites_repr = [
+            "<li><span class='author'>{author}:</span> <span class='text'>{text}</span></li>".format(
+                author=quote['author'], text=quote['text']
+            )
+            for quote in self.favorites
+        ]
+
+        return "".join(favorites_repr)
 
     # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
@@ -34,7 +40,7 @@ class QuoteOfTheDayXBlock(XBlock):
         """
         html = self.resource_string("static/html/quote_of_the_day.html")
         quote, author = read_quote()
-        context = {"quote": quote, "author": author}
+        context = {"quote": quote, "author": author, "favorites": self._render_favorites()}
         frag = Fragment(html.format(self=self, **context))
         frag.add_css(self.resource_string("static/css/quote_of_the_day.css"))
         frag.add_javascript(self.resource_string("static/js/src/quote_of_the_day.js"))
@@ -50,15 +56,16 @@ class QuoteOfTheDayXBlock(XBlock):
     # TO-DO: change this handler to perform your own actions.  You may need more
     # than one handler, or you may not need any handlers at all.
     @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
+    def save_to_favorites(self, data, suffix=''):
         """
-        An example handler, which increments the data.
+        Saves quote to favorites
         """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
-
-        self.count += 1
-        return {"count": self.count}
+        try:
+            quote_author, quote_text = data['author'], data['text']
+            self.favorites.append({"author": quote_author, "text": quote_text})
+            return {"success": True, "message": "Added to favorites!"}
+        except Exception as e:
+            return {"success": False, "message": e.message}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
